5ba5da00de2cd7045b2448ecd60f0affc0707e42
ALI ALRAWI <ali.developer90@gmail.com>
2016-11-25 21:24:06 +0000

    Done.

--- spec/application_integration_spec.rb
+++ spec/application_integration_spec.rb
@@ -1,1 +1,29 @@
 # Write your tests here!
+require 'spec_helper'
+describe "Application" do
+  describe "GET '/'" do
+    before do
+      get '/'
+    end
+    it "returns a 200 status code" do
+      expect(last_response.status).to eq(200)
+    end
+
+
+    it "renders index.erb" do
+      expect(last_response.body).to eq(File.read("views/index.erb"))
+    end
+
+ end
+ describe "POST '/'" do
+   before do
+     post '/greet'
+   end
+   it "returns a 200 status code" do
+     expect(last_response.status).to eq(200)
+   end
+   it "renders greet.erb" do
+     expect(last_response.body).to include("<h1>Hi , nice to meet you!</h1>")
+   end
+ end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,13 @@
 require 'capybara/rspec'
 require 'capybara/dsl'
-
+require 'rack/test'
+RACK_ENV = "test"
+ENV["RACK_ENV"] = "test"
 RSpec.configure do |config|
   config.include Capybara::DSL
   config.order = 'default'
+  config.include Rack::Test::Methods
+
 end
 
 def app
